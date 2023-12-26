b98a0e1cf079e7011a5c3d3498a19fbf3c1e1caa
atere21 <you@tosinateret@gmail.com>
2023-12-26 00:53:16 -0800

    google-auth

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyD1CWYGtQGVhZurMM-472aiJdDAcH8xo1A",
+   authDomain: "auth-39d8c.firebaseapp.com",
+   projectId: "auth-39d8c",
+   storageBucket: "auth-39d8c.appspot.com",
+   messagingSenderId: "677860186061",
+   appId: "1:677860186061:web:db0029c3da44242b751684"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
