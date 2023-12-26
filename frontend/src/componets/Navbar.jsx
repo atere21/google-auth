9195f9a9e93a9df825c91ed34a6a262a5a630587
atere21 <you@tosinateret@gmail.com>
2023-12-26 05:15:56 -0800

    logout implemented

--- frontend/src/componets/Navbar.jsx
+++ frontend/src/componets/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {auth} from '../firebase'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import Signin from './Signin'
+import Logout from './Logout'
 
 const style = {
     nav:`bg-gray-800 h-20 flex justify-between items-center p-4 `,
@@ -14,7 +15,8 @@ const Navbar = () => {
   return (
     <div className={style.nav}>
         <h1 className={style.heading}>Chat App</h1>
-        <Signin/>
+         {user ? <Logout/>: <Signin/>}
+       
     </div>
   )
 }
